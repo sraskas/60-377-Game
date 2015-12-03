2d1f57ec01c0b63ec8d2b27fcbee772a207206c5
Tyler Goetheyn <tyler.goetheyn19@gmail.com>
2015-12-02 20:31:39 -0500

    Basic Set up
    Added in GameController, Character model and one obstacle.
    
    GameController spawns 5 obstacles in a line and a character at the
    beginning

---
+++ FinalProject/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject [] obstacles = new GameObject[5];
+	public GameObject character;
+	private float pos = 0;
+	private float ypos = 0.01f;
+	void Start () {
+
+		foreach (GameObject obstacle in obstacles) {
+			Instantiate (obstacle, new Vector3 (pos, ypos, 0), Quaternion.identity);
+			pos -= 22;
+			ypos += 0.01f;
+		}
+		Instantiate (character, new Vector3 (13.5f, 0.5f, 0.16f), Quaternion.Euler(0, -90f, 0));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
